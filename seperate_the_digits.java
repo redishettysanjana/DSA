7a88249ef2576b99cf3209d9a408bb73ebcd9358
Redishetty Sanjana <redishettysanjana@gmail.com>
2026-07-22 20:10:40 +0530

    worked on seperate the digits in an array problem

---
+++ seperate_the_digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int num : nums) {
+            String a = String.valueOf(num);
+            for (char b : a.toCharArray()) {
+                answer.add(b - '0');
+            }
+        }
+        int[] finalAnswer = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            finalAnswer[i] = answer.get(i);
+        }
+        return finalAnswer;
+    }
+}
