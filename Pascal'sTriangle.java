1ed780feb8bd012c1f36bec45f4687a142277932
Redishetty Sanjana <redishettysanjana@gmail.com>
2026-08-17 18:04:43 +0530

    Solved Pascal's Triangle problem

---
+++ Pascal'sTriangle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+
+            List<Integer> row = new ArrayList<>();
+
+            row.add(1);
+
+            if (i > 0) {
+                for (int j = 1; j < i; j++) {
+                    int value = result.get(i - 1).get(j - 1)
+                               + result.get(i - 1).get(j);
+
+                    row.add(value);
+                }
+
+                row.add(1);
+            }
+
+            result.add(row);
+        }
+
+        return result;
+    }
+}
