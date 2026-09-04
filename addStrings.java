dea9c55719e97d6e2102edddcf906c425f0c86cf
Redishetty Sanjana <redishettysanjana@gmail.com>
2026-09-04 19:58:15 +0530

    Solved Add strings problem

--- addStrings.java
+++ addStrings.java
@@ -3,34 +3,27 @@ class Solution {
 
         int i = num1.length() - 1;
         int j = num2.length() - 1;
-
         int carry = 0;
-
         String result = "";
 
         while (i >= 0 || j >= 0 || carry > 0) {
-
             int digit1 = 0;
             int digit2 = 0;
 
             if (i >= 0) {
                 digit1 = num1.charAt(i) - '0';
             }
-
+            
             if (j >= 0) {
                 digit2 = num2.charAt(j) - '0';
             }
-
             int sum = digit1 + digit2 + carry;
-
             result = (sum % 10) + result;
-
             carry = sum / 10;
 
             i--;
             j--;
         }
-
         return result;
     }
 }
