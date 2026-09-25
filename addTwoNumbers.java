a92d28c092f8ab24c667a954855100bc30e71c92
Redishetty Sanjana <redishettysanjana@gmail.com>
2026-09-25 13:39:45 +0530

    Solved Add Two Numbers problem

---
+++ addTwoNumbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int a = 0;
+            int b = 0;
+
+            if (l1 != null) {
+                a = l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                b = l2.val;
+                l2 = l2.next;
+            }
+
+            int sum = a + b + carry;
+
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+}
